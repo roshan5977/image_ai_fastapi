8c99fa037837daae85a05144121a484d29586e6e
roshan5977 <roshannayak5977@gmail.com>
2023-09-21 12:55:04 +0530

    upload updated

--- main.py
+++ main.py
@@ -26,73 +26,50 @@ def calculate_ssim(image1, image2):
     ssim = cv2.matchTemplate(gray1, gray2, cv2.TM_CCOEFF_NORMED)[0][0]
     return ssim
 
- 
 
- 
 
- 
+def calculate_ssim1(image_1, image_2):
+    print("IMAGE 1",image_1,"\nIMAGE 2", image_2, "-----", end="\n")
+    image_1.file.seek(0)
+    image_2.file.seek(0)
+    image_data1 = np.frombuffer(image_1.file.read(), np.uint8)
+    image1 = cv2.imdecode(image_data1, cv2.IMREAD_COLOR)
+    image_1.file.seek(0)
+    image_2.file.seek(0)
+    image_data2 = np.frombuffer(image_2.file.read(), np.uint8)
+    image2 = cv2.imdecode(image_data2, cv2.IMREAD_COLOR)
+    del image_data1
+    del image_data2
 
- 
+    gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+    
+    ssim = cv2.matchTemplate(gray1, gray2, cv2.TM_CCOEFF_NORMED)[0][0]
+    return ssim
+
+
+
 
 @app.post("/upload/")
-
-async def compare_images(files: list[UploadFile]):
-
-        print("compare_images1",files,len(files))
-
-        if len(files) < 2:
-
+async def compare_images(images: list[UploadFile]):
+        if len(images) < 2:
             raise HTTPException(status_code=400, detail="You must upload at least 2 images for comparison.")
-
- 
-
-        images = []
-
- 
-
-        for file in files:
-
-            # Read and convert uploaded images to numpy arrays
-
-            image_data = np.frombuffer(file.file.read(), np.uint8)
-
-            image = cv2.imdecode(image_data, cv2.IMREAD_COLOR)
-
-            images.append(image)
-
- 
-
-        # Initialize variables to count similar and dissimilar images
-
-        similar_count = 0
-
-        dissimilar_count = 0
-
+        similar_count = 0             
         arr=[]
-
+        duplicate_images = []
         for i in range(len(images)):
-
-            for j in range(len(images)):
-
+            for j in range(i+1,len(images)):
                 if i != j and i not in arr:
-
-                    ssim = calculate_ssim(images[i], images[j])
-
-                    threshold = 0.99999976  # Adjust this threshold as needed
-
+                    print(images[i],images[j],"+++++++++++++++++++++++++++++++++",i,j,end="\n")
+                    ssim = calculate_ssim1(images[i],images[j])
+                    threshold = 0.99999976  
                     print("For image",i,"Comparing with",j,"With SSIM value =",ssim,"********************************")
-
                     if ssim > threshold:
-
+                        duplicate_images.append(images[j].filename)
                         arr.append(j)
-
                         similar_count += 1
 
-           
-
-        dissimilar_count =len(images)-(similar_count+1)
-
-        return {"similar_count": similar_count+1, "dissimilar_count": dissimilar_count}
+        return {"similar_count": similar_count, "duplicate_images": duplicate_images}
 
 @app.post("/compare/")
 async def upload_images(images: list[UploadFile], single_image: UploadFile):
